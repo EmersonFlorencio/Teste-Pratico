87d125f2f907722cc7c301bc665efa9083ea3413
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-02-14 14:07:53 -0300

    feat: iniciando o frontend

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <h1 className='text-3xl text-purple-950 font-bold' >Meu App React</h1>
+    </main>
+  );
+}
