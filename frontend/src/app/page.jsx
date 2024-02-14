35705208179db57c83038a464acc83c4cc0d54ac
EmersonFlorencio <emersonflorencio@hotmail.com>
2024-02-14 17:33:51 -0300

    feat: aplicando a logica de cadatrar e buscar os usuarios de banco

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,18 +1,58 @@
+'use client';
+
 import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { api } from '../services/api';
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const telefoneRef = useRef(null);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const response = await api.get('/api/usuarios');
+    console.log(response.data);
+
+    setUsers(response.data);
+  };
+
+  const hendleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!nameRef.current?.value || !emailRef.current?.value || !telefoneRef.current?.value) return;
+
+    const response = await api.post('/api/usuarios', {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+      telefone: telefoneRef.current?.value
+    });
+
+    setUsers(alluser => [...alluser, response.data]);
+
+    nameRef.current.value = '';
+    emailRef.current.value = '';
+    telefoneRef.current.value = '';
+
+  };
+
   return (
-    <div className="flex min-h-80 h-screen flex-col items-center justify-between p-10">
+    <div className="flex min-h-screen flex-col items-center justify-between p-10">
       <main className='my-10 w-3/12 h-4/5 md:max-w-2x1 bg-indigo-200 gap-5 rounded' >
         <h1 className='text-3xl text-purple-950 font-bold text-center mt-16'>Cadastrar Usuarios</h1>
 
-        <form className='flex flex-col justify-center items-center my-6 '>
+        <form className='flex flex-col justify-center items-center my-6' onSubmit={hendleSubmit}>
           <div className='flex flex-col'>
             <label className='font-medium text-black mb-2'>Nome:</label>
             <input
               type="text"
               placeholder='Nome Completo'
               className='w-72 mb-5 p-2 rounded text-center'
+              ref={nameRef}
             />
           </div>
 
@@ -22,6 +62,7 @@ export default function Home() {
               type="email"
               placeholder='Email'
               className='w-72 mb-5 p-2 rounded text-center'
+              ref={emailRef}
             />
           </div>
 
@@ -31,6 +72,7 @@ export default function Home() {
               type="tel"
               placeholder='Telefone'
               className='w-72 mb-5 p-2 rounded text-center'
+              ref={telefoneRef}
             />
           </div>
 
@@ -45,11 +87,13 @@ export default function Home() {
       </main>
       <section className='flex flex-col'>
         <h2 className='text-3xl text-purple-950 font-bold text-center mb-4' >Lista de Usuarios</h2>
-        <article className='w-96 bg-blue-400 p-2 rounded '>
-          <p><span className='font-medium'>Nome: </span>Emerson</p>
-          <p><span className='font-medium'>Email: </span>Emerson@test.com</p>
-          <p><span className='font-medium'>Telefone: </span>91111-1111</p>
-        </article>
+        {users.map((user) => (
+          <article key={user._id} className='w-96 bg-gray-400 p-2 rounded mt-4'>
+            <p><span className='font-medium'>Nome: </span>{user.name}</p>
+            <p><span className='font-medium'>Email: </span>{user.email}</p>
+            <p><span className='font-medium'>Telefone: </span>{user.telefone}</p>
+          </article>
+        ))}
       </section>
     </div>
   );
